2669f5cded6d0780653fe50f0f11929a12018acd
Jean-Pierre Coetzee <jeanpierrec19@gmail.com>
2022-01-14 23:50:45 +0200

    Fixed RPI cross compiling

--- make-rpi.sh
+++ make-rpi.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export RASPBIAN_ROOTFS=/opt/sysroot/
-export PATH=/opt/cross-pi-gcc/bin:$PATH
-export RASPBERRY_VERSION=3
+# export RASPBIAN_ROOTFS=/opt/sysroot/
+# export PATH=/opt/cross-pi-gcc/bin:$PATH
+# export LD_LIBRARY_PATH=/opt/cross-pi-gcc/lib:$LD_LIBRARY_PATH
+# export RASPBERRY_VERSION=3
 mkdir rpi-build
 cd rpi-build/ && cmake -DCMAKE_TOOLCHAIN_FILE=../Toolchain-rpi.cmake ..
